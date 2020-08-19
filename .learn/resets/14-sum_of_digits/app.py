ccb942475b8996a6bb768df9ed21cb03c44fff6b
Gerdeth <gerdethwickham@gmail.com>
2020-08-19 17:02:05 +0000

    Wed Morning

--- .learn/resets/14-sum_of_digits/app.py
+++ .learn/resets/14-sum_of_digits/app.py
@@ -1,9 +1,6 @@
 #Complete the function "digits_sum" so that it prints the sum of a three digit number.
 def digits_sum(num):
-    new=[]
-    for x in str(num):
-        new.append(int(x))
-    return sum(new)
+  return None
 
 
 #Invoke the function with any three-digit-number
--- .learn/resets/17-Century/app.py
+++ .learn/resets/17-Century/app.py
@@ -1,12 +1,10 @@
 #Complete the function to return the respective number of the century
 #HINT: You may need to import the math module.
-import math 
 
 def century(year):
-    return math.ceil(year/100)
-
+  return None
 
 
 
 #Invoke the function with any given year. 
-print(century(1801))
+print(century())
